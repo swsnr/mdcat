91d703c60fdebd0e84da1315720bcb0a591a75f9
Sebastian Wiesner <sebastian@swsnr.de>
2018-01-07 12:24:15 +0100

    Add file name argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+// Copyright 2018 Sebastian Wiesner <sebastian@swsnr.de>
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Arguments {
+    #[structopt(help = "Input file.  If absent or - read from standard input")]
+    filename: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::from_args();
+    println!("Read from {:?}", args.filename);
 }
