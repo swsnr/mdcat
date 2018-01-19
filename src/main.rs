083150be78d3b845f4b9b37eda23ed23f6024624
Sebastian Wiesner <sebastian@swsnr.de>
2018-01-19 16:12:22 +0100

    Deny all warnings and clippy lints

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![deny(warnings)]
+#![deny(missing_docs)]
+#![cfg_attr(feature = "cargo-clippy", deny(clippy))]
+
+//! Show CommonMark documents on TTYs.
+
 extern crate base64;
 extern crate pulldown_cmark;
 extern crate structopt;
