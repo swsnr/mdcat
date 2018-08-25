c0dc3d4b52ec483c512044241ad012a77dc87eb0
Sebastian Wiesner <sebastian@swsnr.de>
2018-08-25 14:26:44 +0200

    Extract base terminal types

---
+++ src/terminal/types.rs
@@ -0,0 +1,99 @@
+// Copyright 2018 Sebastian Wiesner <sebastian@swsnr.de>
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Basic terminal types
+
+use std;
+use term_size;
+
+/// The size of a text terminal.
+#[derive(Debug, Copy, Clone)]
+pub struct Size {
+    /// The width of the terminal, in characters aka columns.
+    pub width: usize,
+    /// The height of the terminal, in lines.
+    pub height: usize,
+}
+
+impl Default for Size {
+    /// A good default size assumption for a terminal: 80x24.
+    fn default() -> Size {
+        Size {
+            width: 80,
+            height: 24,
+        }
+    }
+}
+
+impl Size {
+    fn new(width: usize, height: usize) -> Size {
+        Size { width, height }
+    }
+
+    /// Get terminal size from `$COLUMNS` and `$LINES`.
+    fn from_env() -> Option<Size> {
+        let columns = std::env::var("COLUMNS")
+            .ok()
+            .and_then(|value| value.parse::<usize>().ok());
+        let rows = std::env::var("LINES")
+            .ok()
+            .and_then(|value| value.parse::<usize>().ok());
+
+        match (columns, rows) {
+            (Some(columns), Some(rows)) => Some(Size::new(columns, rows)),
+            _ => None,
+        }
+    }
+
+    /// Detect the terminal size.
+    ///
+    /// Get the terminal size from the underlying TTY, and fallback to
+    /// `$COLUMNS` and `$LINES`.
+    pub fn detect() -> Option<Size> {
+        term_size::dimensions()
+            .map(|(w, h)| Size::new(w, h))
+            .or_else(Size::from_env)
+    }
+}
+
+/// An ANSI colour.
+#[derive(Debug, Copy, Clone)]
+#[allow(dead_code)]
+pub enum AnsiColour {
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    LightRed,
+    LightGreen,
+    LightYellow,
+    LightBlue,
+    LightMagenta,
+    LightCyan,
+}
+
+/// An ANSI style to enable on a terminal.
+#[derive(Debug, Copy, Clone)]
+#[allow(dead_code)]
+pub enum AnsiStyle {
+    Reset,
+    Bold,
+    Italic,
+    NoItalic,
+    Underline,
+    Foreground(AnsiColour),
+    DefaultForeground,
+}
