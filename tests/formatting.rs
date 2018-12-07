9c84a48ff8e94ec5ec2f3ad4cac189a81057b1ef
Sebastian Wiesner <sebastian@swsnr.de>
2018-12-07 20:44:55 +0100

    Deny all warnings and lints for formatting tests

--- tests/formatting.rs
+++ tests/formatting.rs
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![deny(warnings, missing_docs, clippy::all)]
+// Warn about deprecated trait object syntax
+#![deny(bare_trait_objects)]
+
 use mdcat;
 
 #[macro_use]
