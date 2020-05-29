swsnr/mdcat

commit 20a51343607b3655c01fba27548ec56969920550

Simplify error handling
Use anyhow::Error instead of Box<dyn Error> as catch-all error type and
cut down external errors to std::io::Error; mdcat never fails unless it
can't write.

Closes GH-139

src/resources.rs
@@ -6,9 +6,10 @@
 
 //! Access to resources referenced from markdown documents.
 
+use anyhow::{anyhow, Context, Error, Result};
+use fehler::{throw, throws};
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::{Error, ErrorKind};
 use url::Url;
 
 /// What kind of resources mdcat may access when rendering.
@@ -40,40 +41,42 @@ fn is_local(url: &Url) -> bool {
 }
 
 #[cfg(feature = "reqwest")]
-fn fetch_http(url: &Url) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let mut response = reqwest::blocking::get(url.clone())?;
+#[throws]
+fn fetch_http(url: &Url) -> Vec<u8> {
+    let mut response =
+        reqwest::blocking::get(url.clone()).with_context(|| format!("Failed to GET {}", url))?;
     if response.status().is_success() {
         let mut buffer = Vec::new();
-        response.read_to_end(&mut buffer)?;
-        Ok(buffer)
+        response
+            .read_to_end(&mut buffer)
+            .with_context(|| format!("Failed to read from URL {}", url))?;
+        buffer
     } else {
-        Err(Error::new(
-            ErrorKind::Other,
-            format!("HTTP error status {} by GET {}", response.status(), url),
-        )
-        .into())
+        throw!(anyhow!(
+            "HTTP error status {} by GET {}",
+            response.status(),
+            url
+        ))
     }
 }
 
 #[cfg(not(feature = "reqwest"))]
-fn fetch_http(url: &Url) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+#[throws]
+fn fetch_http(url: &Url) -> Vec<u8> {
     let output = std::process::Command::new("curl")
         .arg("-fsSL")
         .arg(url.to_string())
-        .output()?;
+        .output()
+        .with_context(|| format!("curl -fsSL {} failed to spawn", url))?;
 
     if output.status.success() {
-        Ok(output.stdout)
+        output.stdout
     } else {
-        Err(Error::new(
-            ErrorKind::Other,
-            format!(
-                "curl {} failed: {}",
-                url,
-                String::from_utf8_lossy(&output.stderr)
-            ),
-        )
-        .into())
+        throw!(anyhow!(
+            "curl -fsSL {} failed: {}",
+            url,
+            String::from_utf8_lossy(&output.stderr)
+        ))
     }
 }
 
@@ -85,26 +88,21 @@ fn fetch_http(url: &Url) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
 /// We currently support `file:` URLs which the underlying operation system can
 /// read (local on UNIX, UNC paths on Windows), and HTTP(S) URLs if enabled at
 /// build system.
-pub fn read_url(url: &Url) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+pub fn read_url(url: &Url) -> Result<Vec<u8>> {
     match url.scheme() {
         "file" => match url.to_file_path() {
             Ok(path) => {
                 let mut buffer = Vec::new();
-                File::open(path)?.read_to_end(&mut buffer)?;
+                File::open(path)
+                    .with_context(|| format!("Failed to open file at {}", url))?
+                    .read_to_end(&mut buffer)
+                    .with_context(|| format!("Failed to read from file at {}", url))?;
                 Ok(buffer)
             }
-            Err(_) => Err(Error::new(
-                ErrorKind::InvalidInput,
-                format!("Remote file: URL {} not supported", url),
-            )
-            .into()),
+            Err(_) => Err(anyhow!("Cannot convert URL {} to file path", url)),
         },
         "http" | "https" => fetch_http(url),
-        _ => Err(Error::new(
-            ErrorKind::InvalidInput,
-            format!("Protocol of URL {} not supported", url),
-        )
-        .into()),
+        _ => Err(anyhow!("Cannot read from URL {}, protocol not supported")),
     }
 }
 

Cargo.toml
@@ -24,6 +24,7 @@ mime = "^0.3"
 term_size = "^0.3"
 url = "^2.1"
 fehler = "^1"
+anyhow = "^1"
 
 # Dependency for reqwest which lets Kitty, iTerm2 and perhaps others
 # fetch images from HTTP(S) URLs for display.  Without reqwest we use curl to

src/lib.rs
@@ -21,7 +21,9 @@ mod svg;
 mod terminal;
 
 mod render;
-pub use render::Error;
+
+/// The catch-all error type returned by mdcat.
+pub type Error = std::io::Error;
 
 // Expose some select things for use in main
 pub use crate::resources::ResourceAccess;
@@ -106,7 +108,7 @@ mod tests {
     use super::*;
     use pulldown_cmark::Parser;
 
-    #[throws]
+    #[throws(anyhow::Error)]
     fn render_string(input: &str, settings: &Settings) -> String {
         let source = Parser::new(input);
         let mut sink = Vec::new();
@@ -117,11 +119,11 @@ mod tests {
     mod layout {
         use super::render_string;
         use crate::*;
+        use anyhow::Result;
         use pretty_assertions::assert_eq;
-        use std::error::Error;
         use syntect::parsing::SyntaxSet;
 
-        fn render(markup: &str) -> Result<String, Box<dyn Error>> {
+        fn render(markup: &str) -> Result<String> {
             render_string(
                 markup,
                 &Settings {

src/magic.rs
@@ -6,9 +6,10 @@
 
 //! Magic util functions for detecting image types.
 
+use anyhow::{anyhow, Context, Result};
 use mime::Mime;
 use std::io::prelude::*;
-use std::io::{Error, ErrorKind};
+use std::io::ErrorKind;
 use std::process::*;
 
 /// Whether the given MIME type denotes an SVG image.
@@ -22,7 +23,7 @@ pub fn is_png(mime: &Mime) -> bool {
 }
 
 /// Detect mime type with `file`.
-pub fn detect_mime_type(buffer: &[u8]) -> Result<Mime, Box<dyn std::error::Error>> {
+pub fn detect_mime_type(buffer: &[u8]) -> Result<Mime> {
     let mut process = Command::new("file")
         .arg("--brief")
         .arg("--mime-type")
@@ -30,7 +31,8 @@ pub fn detect_mime_type(buffer: &[u8]) -> Result<Mime, Box<dyn std::error::Error
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
-        .spawn()?;
+        .spawn()
+        .with_context(|| "Failed to spawn mime --brief --mime-type")?;
 
     process
         .stdin
@@ -42,22 +44,27 @@ pub fn detect_mime_type(buffer: &[u8]) -> Result<Mime, Box<dyn std::error::Error
             _ => Err(error),
         })?;
 
-    let output = process.wait_with_output()?;
+    let output = process
+        .wait_with_output()
+        .with_context(|| "Failed to read output from mime --brief --mime-type")?;
     if output.status.success() {
-        std::str::from_utf8(&output.stdout)?
-            .trim()
+        let stdout = std::str::from_utf8(&output.stdout)
+            .with_context(|| {
+                format!(
+                    "mime --brief --mime-type returned non-utf8: {:?}",
+                    output.stdout
+                )
+            })?
+            .trim();
+        stdout
             .parse()
-            .map_err(Into::into)
+            .with_context(|| format!("Failed to parse mime type from output: {}", stdout))
     } else {
-        Err(Error::new(
-            ErrorKind::Other,
-            format!(
-                "file --brief --mime-type failed with status {}: {}",
-                output.status,
-                String::from_utf8_lossy(&output.stderr)
-            ),
-        )
-        .into())
+        Err(anyhow!(
+            "file --brief --mime-type failed with status {}: {}",
+            output.status,
+            String::from_utf8_lossy(&output.stderr)
+        ))
     }
 }
 

src/main.rs
@@ -9,12 +9,13 @@
 //! Show CommonMark documents on TTYs.
 
 use clap::{value_t, values_t};
+use fehler::throws;
 use mdcat::Settings;
 use pulldown_cmark::{Options, Parser};
-use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{stdin, stdout};
+use std::io::{Error, Result};
 use std::path::PathBuf;
 use syntect::parsing::SyntaxSet;
 
@@ -24,13 +25,14 @@ use mdcat::{ResourceAccess, TerminalCapabilities, TerminalSize};
 ///
 /// If `filename` is `-` read from standard input, otherwise try to open and
 /// read the given file.
-fn read_input<T: AsRef<str>>(filename: T) -> std::io::Result<(PathBuf, String)> {
+#[throws]
+fn read_input<T: AsRef<str>>(filename: T) -> (PathBuf, String) {
     let cd = std::env::current_dir()?;
     let mut buffer = String::new();
 
     if filename.as_ref() == "-" {
         stdin().read_to_string(&mut buffer)?;
-        Ok((cd, buffer))
+        (cd, buffer)
     } else {
         let mut source = File::open(filename.as_ref())?;
         source.read_to_string(&mut buffer)?;
@@ -39,15 +41,11 @@ fn read_input<T: AsRef<str>>(filename: T) -> std::io::Result<(PathBuf, String)> 
             .parent()
             .map(|p| p.to_path_buf())
             .unwrap_or(cd);
-        Ok((base_dir, buffer))
+        (base_dir, buffer)
     }
 }
 
-fn process_file(
-    filename: &str,
-    settings: &Settings,
-    dump_events: bool,
-) -> Result<(), Box<dyn Error>> {
+fn process_file(filename: &str, settings: &Settings, dump_events: bool) -> Result<()> {
     let (base_dir, input) = read_input(filename)?;
     let mut options = Options::empty();
     options.insert(Options::ENABLE_TASKLISTS);
@@ -59,9 +57,12 @@ fn process_file(
     } else {
         mdcat::push_tty(settings, &mut stdout(), &base_dir, parser)
     }
-    .or_else(|error| match error.downcast_ref::<std::io::Error>() {
-        Some(error) if error.kind() == std::io::ErrorKind::BrokenPipe => Ok(()),
-        _ => Err(error),
+    .or_else(|error| {
+        if error.kind() == std::io::ErrorKind::BrokenPipe {
+            Ok(())
+        } else {
+            Err(error)
+        }
     })
 }
 
@@ -140,7 +141,7 @@ markdown documents for viewing in text terminals:
 
 Copyright (C) Sebastian Wiesner and contributors
 
-This program is subject to the terms of the Mozilla Public License, 
+This program is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file,
 You can obtain one at http://mozilla.org/MPL/2.0/.
 

src/render/mod.rs
@@ -14,6 +14,7 @@ use fehler::throws;
 use pulldown_cmark::Event::*;
 use pulldown_cmark::Tag::*;
 use pulldown_cmark::{Event, LinkType};
+use std::io::Error;
 use syntect::highlighting::{HighlightIterator, Highlighter, Theme};
 use syntect::util::LinesWithEndings;
 use url::Url;
@@ -37,9 +38,6 @@ fn resolve_reference(base_dir: &Path, reference: &str) -> Option<Url> {
         .or_else(|_| Url::from_file_path(base_dir.join(reference)))
         .ok()
 }
-
-/// A rendering error.
-pub type Error = Box<dyn std::error::Error>;
 
 #[allow(clippy::cognitive_complexity)]
 #[throws]
@@ -589,6 +587,7 @@ pub fn write_event<'a, W: Write>(
                         iterm2.write_inline_image(writer, url.as_str(), &contents)?;
                         Ok(RenderedImage)
                     })
+                    .map(|_| RenderedImage)
                     .ok(),
                 (Kitty(ref kitty), Some(ref url)) => kitty
                     .read_and_render(url)

src/terminal/iterm2.rs
@@ -14,7 +14,7 @@
 use super::osc::write_osc;
 use crate::magic;
 use crate::resources::read_url;
-use std::error::Error;
+use anyhow::{Context, Result};
 use std::ffi::OsStr;
 use std::io::{self, Write};
 use url::Url;
@@ -81,10 +81,13 @@ impl ITerm2Images {
     ///
     /// Render the binary content of the (rendered) image or an IO error if
     /// reading or rendering failed.
-    pub fn read_and_render(self, url: &Url) -> Result<Vec<u8>, Box<dyn Error>> {
+    pub fn read_and_render(self, url: &Url) -> Result<Vec<u8>> {
         let contents = read_url(&url)?;
-        if magic::is_svg(&magic::detect_mime_type(&contents)?) {
-            svg::render_svg(&contents).map_err(Into::into)
+        let mimetype = magic::detect_mime_type(&contents)
+            .with_context(|| format!("Failed to guess mime type for URL {}", url))?;
+        if magic::is_svg(&mimetype) {
+            svg::render_svg(&contents)
+                .with_context(|| format!("Failed to render SVG at URL {}", url))
         } else {
             Ok(contents)
         }

src/terminal/kitty.rs
@@ -22,10 +22,11 @@
 use crate::magic;
 use crate::resources::read_url;
 use crate::svg::render_svg;
+use anyhow::{anyhow, Context, Error, Result};
+use fehler::throws;
 use image::imageops::FilterType;
 use image::ColorType;
 use image::{DynamicImage, GenericImageView};
-use std::error::Error;
 use std::io::Write;
 use std::process::{Command, Stdio};
 use std::str;
@@ -46,57 +47,44 @@ pub fn is_kitty() -> bool {
 ///
 /// We cannot use the terminal size information from Context.output.size, because
 /// the size information are in columns / rows instead of pixel.
-fn get_terminal_size() -> std::io::Result<KittyDimension> {
-    use std::io::{Error, ErrorKind};
-
+fn get_terminal_size() -> Result<KittyDimension> {
     let process = Command::new("kitty")
         .arg("+kitten")
         .arg("icat")
         .arg("--print-window-size")
         .stdout(Stdio::piped())
-        .spawn()?;
+        .spawn()
+        .with_context(|| "Failed to spawn kitty +kitten icat --print-window-size")?;
 
     let output = process.wait_with_output()?;
 
     if output.status.success() {
-        let terminal_size_str = std::str::from_utf8(&output.stdout).or_else(|_| {
-            Err(Error::new(
-                ErrorKind::Other,
-                "The terminal size could not be read.".to_string(),
-            ))
+        let terminal_size_str = std::str::from_utf8(&output.stdout).with_context(|| {
+            format!(
+                "kitty +kitten icat --print-window-size returned non-utf8: {:?}",
+                output.stdout
+            )
         })?;
         let terminal_size = terminal_size_str.split('x').collect::<Vec<&str>>();
 
-        let (width, height) = (
-            terminal_size[0].parse::<u32>().or_else(|_| {
-                Err(Error::new(
-                    ErrorKind::Other,
-                    format!(
-                        "The terminal width could not be parsed: {}",
-                        terminal_size_str
-                    ),
-                ))
-            })?,
-            terminal_size[1].parse::<u32>().or_else(|_| {
-                Err(Error::new(
-                    ErrorKind::Other,
-                    format!(
-                        "The terminal height could not be parsed: {}",
-                        terminal_size_str
-                    ),
-                ))
-            })?,
-        );
-
-        Ok(KittyDimension { width, height })
+        terminal_size[0]
+            .parse::<u32>()
+            .and_then(|width| {
+                terminal_size[1]
+                    .parse::<u32>()
+                    .map(|height| KittyDimension { width, height })
+            })
+            .with_context(|| {
+                format!(
+                    "Failed to parse kitty width and height from output: {}",
+                    terminal_size_str
+                )
+            })
     } else {
-        Err(Error::new(
-            ErrorKind::Other,
-            format!(
-                "kitty +kitten icat --print-window-size failed with status {}: {}",
-                output.status,
-                String::from_utf8_lossy(&output.stderr)
-            ),
+        Err(anyhow!(
+            "kitty +kitten icat --print-window-size failed with status {}: {}",
+            output.status,
+            String::from_utf8_lossy(&output.stderr)
         ))
     }
 }
@@ -107,11 +95,8 @@ pub struct KittyImages;
 
 impl KittyImages {
     /// Write an inline image for kitty.
-    pub fn write_inline_image<W: Write>(
-        self,
-        writer: &mut W,
-        image: KittyImage,
-    ) -> Result<(), Box<dyn Error>> {
+    #[throws]
+    pub fn write_inline_image<W: Write>(self, writer: &mut W, image: KittyImage) -> () {
         // Kitty's escape sequence is like: Put the command key/value pairs together like "{}={}(,*)"
         // and write them along with the image bytes in 4096 bytes chunks to the stdout.
         // Documentation gives the following python example:
@@ -175,20 +160,25 @@ impl KittyImages {
 
             cmd_header.clear();
         }
-
-        Ok(())
     }
 
     /// Read the image bytes from the given URL and wrap them in a `KittyImage`.
     /// It scales the image down, if the image size exceeds the terminal window size.
-    pub fn read_and_render(self, url: &Url) -> Result<KittyImage, Box<dyn std::error::Error>> {
+    #[throws]
+    pub fn read_and_render(self, url: &Url) -> KittyImage {
         let contents = read_url(url)?;
-        let mime = magic::detect_mime_type(&contents)?;
+        let mime = magic::detect_mime_type(&contents)
+            .with_context(|| format!("Failed to detect mime type for URL {}", url))?;
         let image = if magic::is_svg(&mime) {
-            image::load_from_memory(&render_svg(&contents)?)
+            image::load_from_memory(
+                &render_svg(&contents)
+                    .with_context(|| format!("Failed to render SVG at {} to PNG", url))?,
+            )
+            .with_context(|| format!("Failed to load SVG rendered from {}", url))?
         } else {
             image::load_from_memory(&contents)
-        }?;
+                .with_context(|| format!("Failed to load image from URL {}", url))?
+        };
         let terminal_size = get_terminal_size()?;
 
         if magic::is_png(&mime) && terminal_size.contains(&image.dimensions().into()) {
@@ -199,12 +189,12 @@ impl KittyImages {
     }
 
     /// Wrap the image bytes as PNG format in `KittyImage`.
-    fn render_as_png(self, contents: Vec<u8>) -> Result<KittyImage, Box<dyn Error>> {
-        Ok(KittyImage {
+    fn render_as_png(self, contents: Vec<u8>) -> KittyImage {
+        KittyImage {
             contents,
             format: KittyFormat::PNG,
             dimension: None,
-        })
+        }
     }
 
     /// Render the image as RGB/RGBA format and wrap the image bytes in `KittyImage`.
@@ -213,7 +203,7 @@ impl KittyImages {
         self,
         image: DynamicImage,
         terminal_size: KittyDimension,
-    ) -> Result<KittyImage, Box<dyn Error>> {
+    ) -> KittyImage {
         let format = match image.color() {
             ColorType::L8
             | ColorType::Rgb8
@@ -239,14 +229,14 @@ impl KittyImages {
 
         let image_dimension = image.dimensions().into();
 
-        Ok(KittyImage {
+        KittyImage {
             contents: match format {
                 KittyFormat::RGB => image.into_rgb().into_raw(),
                 _ => image.into_rgba().into_raw(),
             },
             format,
             dimension: Some(image_dimension),
-        })
+        }
     }
 }
 

commit c7390561e97d1aab3a8a8b1e347a07b7dfe10214

Fix read_url test for curl
Update expected message to match the anyhow-based message we have now.

Regression from 20a51343607b3655c01fba27548ec56969920550

src/resources.rs
@@ -170,12 +170,12 @@ mod tests {
             )
         } else {
             assert!(
-                error.contains("curl https://eu.httpbin.org/status/404 failed:"),
+                error.contains("curl -fsSL https://eu.httpbin.org/status/404 failed:"),
                 "Error did not contain expected string: {}",
                 error
             );
             assert!(
-                error.contains("404"),
+                error.contains("URL returned error: 404"),
                 "Error did not contain expected string: {}",
                 error
             );