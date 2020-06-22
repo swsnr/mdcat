c167b35012243927422750de674c93d398e6918a
Sebastian Wiesner <sebastian@swsnr.de>
2020-06-22 10:35:01 +0200

    Replace system facts in golden files
    OSC 8 links for file URLs include the hostname and the absolute path of
    the working directory; we need to replace these in the output before
    writing the golden file so that our tests work everywhere.

--- tests/render.rs
+++ tests/render.rs
@@ -17,8 +17,9 @@ use pulldown_cmark::{Options, Parser};
 use syntect::parsing::SyntaxSet;
 use test_generator::test_resources;
 
-use anyhow::Context;
+use anyhow::{Context, Result};
 use lazy_static::lazy_static;
+use std::io::Write;
 
 lazy_static! {
     static ref SYNTAX_SET: SyntaxSet = SyntaxSet::load_defaults_newlines();
@@ -36,17 +37,64 @@ lazy_static! {
     };
 }
 
+fn render<P: AsRef<Path>>(markdown_file: P, settings: &mdcat::Settings) -> Result<String> {
+    let markdown = std::fs::read_to_string(&markdown_file).with_context(|| {
+        format!(
+            "Failed to read markdown from {}",
+            markdown_file.as_ref().display()
+        )
+    })?;
+    let parser = Parser::new_ext(
+        &markdown,
+        Options::ENABLE_TASKLISTS | Options::ENABLE_STRIKETHROUGH,
+    );
+    let abs_path = std::fs::canonicalize(&markdown_file).with_context(|| {
+        format!(
+            "Failed to convert {} to an absolute path",
+            markdown_file.as_ref().display()
+        )
+    })?;
+    let base_dir = abs_path
+        .parent()
+        .expect("Absolute file name must have a parent!");
+    let mut sink = Vec::new();
+    mdcat::push_tty(settings, &mut sink, base_dir, parser).with_context(|| {
+        format!(
+            "Failed to render contents of {}",
+            markdown_file.as_ref().display()
+        )
+    })?;
+    String::from_utf8(sink).with_context(|| format!("Failed to convert rendered result to string"))
+}
+
 fn render_golden_file<P: AsRef<Path>>(
     golden_dir: P,
     markdown_file: &str,
     settings: &mdcat::Settings,
 ) -> () {
+    let hostname = gethostname::gethostname();
+    let cwd = std::fs::canonicalize(std::env::current_dir().expect("Require working directory"))
+        .expect("Canonical working directory");
+    // Replace environment specific facts in
+    let rendered = render(markdown_file, settings)
+        .unwrap()
+        .replace(
+            &format!(
+                "file://{}/",
+                hostname.to_str().expect("Non-string hostname")
+            ),
+            "file://HOSTNAME/",
+        )
+        .replace(
+            cwd.to_str().expect("Non-string working directory"),
+            "/WORKING_DIR",
+        );
+
     let prefix = "tests/render/md";
     let golden_path = Path::new(markdown_file)
         .strip_prefix(prefix)
         .with_context(|| format!("Failed to strip {} from {}", prefix, markdown_file))
-        .unwrap()
-        .with_extension("");
+        .unwrap();
 
     let mut mint = goldenfile::Mint::new(
         golden_dir.as_ref().join(
@@ -55,14 +103,6 @@ fn render_golden_file<P: AsRef<Path>>(
                 .with_context(|| format!("Failed to get parent of {}", golden_path.display()))
                 .unwrap(),
         ),
-    );
-
-    let markdown = std::fs::read_to_string(markdown_file)
-        .with_context(|| format!("Failed to read markdown file from {}", markdown_file))
-        .unwrap();
-    let parser = Parser::new_ext(
-        &markdown,
-        Options::ENABLE_TASKLISTS | Options::ENABLE_STRIKETHROUGH,
     );
     let mut golden = mint
         .new_goldenfile(
@@ -73,17 +113,10 @@ fn render_golden_file<P: AsRef<Path>>(
         )
         .with_context(|| format!("Failed to open golden file at {}", golden_path.display()))
         .unwrap();
-    let base_dir = std::env::current_dir()
-        .expect("Current dir not available")
-        .join(
-            &Path::new(markdown_file)
-                .parent()
-                .with_context(|| format!("Markdown file {} had no parent", markdown_file))
-                .unwrap(),
-        );
-    mdcat::push_tty(settings, &mut golden, &base_dir, parser)
-        .with_context(|| format!("Failed to render {}", markdown_file))
-        .unwrap()
+    golden
+        .write_all(rendered.as_bytes())
+        .with_context(|| format!("Failed to write to {}", golden_path.display()))
+        .unwrap();
 }
 
 #[test_resources("tests/render/md/*/*.md")]
