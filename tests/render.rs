51d40387dfefebfd29ae2bdc3f0d934ec4b10083
Sebastian Wiesner <sebastian@swsnr.de>
2021-05-01 08:15:26 +0200

    Drop goldenfile dependency
    It's trivial to replace and not exactly enthusiastically maintained, see
    https://github.com/calder/rust-goldenfile/issues/4

--- tests/render.rs
+++ tests/render.rs
@@ -9,10 +9,11 @@
 //! Test a set of custom samples as well as all samples from the official common mark spec against
 //! known golden files to catch rendering regressions.
 
-// #![deny(warnings, missing_docs, clippy::all)]
+#![deny(warnings, missing_docs, clippy::all)]
 
 use std::path::Path;
 
+use pretty_assertions::assert_eq;
 use pulldown_cmark::{Options, Parser};
 use syntect::parsing::SyntaxSet;
 use test_generator::test_resources;
@@ -20,7 +21,6 @@ use test_generator::test_resources;
 use anyhow::{Context, Result};
 use lazy_static::lazy_static;
 use mdcat::Environment;
-use std::io::Write;
 use url::Url;
 
 lazy_static! {
@@ -45,7 +45,10 @@ lazy_static! {
     };
 }
 
-fn render<P: AsRef<Path>>(markdown_file: P, settings: &mdcat::Settings) -> Result<String> {
+fn render_to_string<P: AsRef<Path>>(
+    markdown_file: P,
+    settings: &mdcat::Settings,
+) -> Result<String> {
     let markdown = std::fs::read_to_string(&markdown_file).with_context(|| {
         format!(
             "Failed to read markdown from {}",
@@ -105,49 +108,51 @@ fn replace_system_specific_urls(input: String) -> String {
     })
 }
 
-fn render_golden_file<P: AsRef<Path>>(
-    golden_dir: P,
-    markdown_file: &str,
+fn test_with_golden_file<S: AsRef<Path>, T: AsRef<Path>>(
+    markdown_file: S,
+    golden_file_directory: T,
     settings: &mdcat::Settings,
 ) {
     // Replace environment specific facts in
-    let rendered = replace_system_specific_urls(render(markdown_file, settings).unwrap());
+    let actual =
+        replace_system_specific_urls(render_to_string(markdown_file.as_ref(), settings).unwrap());
 
-    let prefix = "tests/render/md";
-    let golden_path = Path::new(markdown_file)
-        .strip_prefix(prefix)
-        .with_context(|| format!("Failed to strip {} from {}", prefix, markdown_file))
-        .unwrap();
+    let basename = markdown_file
+        .as_ref()
+        .strip_prefix("tests/render/md")
+        .with_context(|| {
+            format!(
+                "Failed to strip prefix from {}",
+                markdown_file.as_ref().display()
+            )
+        })
+        .unwrap()
+        .with_extension("");
+    let expected_file = golden_file_directory.as_ref().join(basename);
 
-    let mut mint = goldenfile::Mint::new(
-        golden_dir.as_ref().join(
-            golden_path
-                .parent()
-                .with_context(|| format!("Failed to get parent of {}", golden_path.display()))
-                .unwrap(),
-        ),
-    );
-    let mut golden = mint
-        .new_goldenfile(
-            golden_path
-                .file_stem()
-                .with_context(|| format!("Failed to get stem of {}", golden_path.display()))
-                .unwrap(),
-        )
-        .with_context(|| format!("Failed to open golden file at {}", golden_path.display()))
-        .unwrap();
-    golden
-        .write_all(rendered.as_bytes())
-        .with_context(|| format!("Failed to write to {}", golden_path.display()))
-        .unwrap();
+    if std::env::var_os("MDCAT_UPDATE_GOLDEN_FILES").is_some() {
+        std::fs::write(&expected_file, &actual)
+            .with_context(|| {
+                format!(
+                    "Failed to update golden file at {}",
+                    expected_file.display()
+                )
+            })
+            .unwrap()
+    } else {
+        let expected = std::fs::read_to_string(&expected_file)
+            .with_context(|| format!("Failed to read golden file at {}", expected_file.display()))
+            .unwrap();
+        assert_eq!(actual, expected);
+    }
 }
 
 /// Test basic rendering.
 #[test_resources("tests/render/md/*/*.md")]
 fn ansi_only(markdown_file: &str) {
-    render_golden_file(
-        "tests/render/golden/ansi-only",
+    test_with_golden_file(
         markdown_file,
+        "tests/render/golden/ansi-only",
         &*SETTINGS_ANSI_ONLY,
     )
 }
@@ -155,16 +160,16 @@ fn ansi_only(markdown_file: &str) {
 /// Test basic rendering plus inline links.
 #[test_resources("tests/render/md/*/*.md")]
 fn vte50(markdown_file: &str) {
-    render_golden_file("tests/render/golden/vte50", markdown_file, &*SETTINGS_VTE50)
+    test_with_golden_file(markdown_file, "tests/render/golden/vte50", &*SETTINGS_VTE50)
 }
 
 /// Test the full shebang, but not on Windows, since the iTerm2 backend has some unimplemented stuff on Windows.
 #[test_resources("tests/render/md/*/*.md")]
 #[cfg(not(windows))]
 fn iterm2(markdown_file: &str) {
-    render_golden_file(
-        "tests/render/golden/iterm2",
+    test_with_golden_file(
         markdown_file,
+        "tests/render/golden/iterm2",
         &*SETTINGS_ITERM2,
     )
 }
